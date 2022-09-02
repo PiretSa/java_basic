b2c821d11cc0d510db41b7d9fa77883092824037
Piret <piret.saar.003@khk.ee>
2022-09-02 23:14:58 +0300

    Week 1 - Ex16

---
+++ src/week1/Ex16.java
@@ -0,0 +1,18 @@
+package week1;
+import java.util.Scanner;
+
+public class Ex16 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Type a number: ");
+        int number = reader.nextInt();
+
+        if(number % 2 == 0)
+            System.out.println("Number " + number + " is even");
+        else
+            System.out.println("Number " + number + " is odd");
+
+        System.out.println();
+    }
+}
