65dd5199c603432b3e32286f8e2fe1e055d0f177
Piret <piret.saar.003@khk.ee>
2022-09-05 10:26:55 +0300

    Week 1 - Ex21

---
+++ src/week1/Ex21.java
@@ -0,0 +1,18 @@
+package week1;
+import java.util.Scanner;
+
+public class Ex21 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Type a year: ");
+        int year = reader.nextInt();
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println("The year is a leap year");
+        } else {
+            System.out.println("The year is not a leap year");
+        }
+
+    }
+}
