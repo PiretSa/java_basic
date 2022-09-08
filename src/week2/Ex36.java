70650712ea5e95e949bdcea9b4a40477fe5300e1
Piret <piret.saar.003@khk.ee>
2022-09-08 12:24:53 +0300

    Week 2 - Ex36 Sum of the numbers

--- src/week2/Ex36.java
+++ src/week2/Ex36.java
@@ -8,14 +8,17 @@ public class Ex36 {
         Scanner reader = new Scanner(System.in);
 
         int number;
+        int sum = 0;
+
         while (true) {
             number = Integer.parseInt(reader.nextLine());
 
             if (number ==-1) {
                 break;  // end the loop
             }
+            sum += number;
         }
-
+        System.out.println("Sum of numbers is: " + sum);
         System.out.println("Thank you and see you later");
 
     }
