bab4bc5f09718d5f2d45b20219a6f94c2ce8818f
Piret <piret.saar.003@khk.ee>
2022-09-09 11:23:22 +0300

    Week 4 - Ex79 Many sums

--- src/week4/Ex79.java
+++ src/week4/Ex79.java
@@ -1,15 +1,35 @@
 package week4;
+import java.util.Scanner;
 
 public class Ex79 {
         public static void main(String[] args) {
             NumberStatistics stats = new NumberStatistics();
-            stats.addNumber(3);
-            stats.addNumber(5);
-            stats.addNumber(1);
-            stats.addNumber(2);
-            stats.addNumber(6);
-            System.out.println("Amount: " + stats.amountOfNumbers());
-            System.out.println("sum: " + stats.sum());
-            System.out.println("average: " + stats.average());
+            Scanner reader = new Scanner(System.in);
+
+            System.out.println("Type a number: ");
+            int number;
+            int sum = 0;
+            int count = 0;
+            int even = 0;
+            int odd = 0;
+
+            while (true) {
+                number = Integer.parseInt(reader.nextLine());
+
+                if (number == -1) {
+                    break;  // end the loop
+                }
+                sum += number;
+                count++;
+                if(number % 2 == 0){
+                    even++;
+                } else {
+                    odd++;
+                }
+            }
+
+            System.out.println("sum of numbers is:" + sum);
+            System.out.println("sum of even: " + even);
+            System.out.println("sum of odd: " + odd);
         }
     }
