8c79b24f8454e6bb1dc678c0204b71810604058a
Piret <piret.saar.003@khk.ee>
2022-09-09 22:16:43 +0300

    Week 2 - Ex26 Sum of many numbers

---
+++ src/week2/Ex26.java
@@ -0,0 +1,24 @@
+package week2;
+
+import java.util.Scanner;
+
+public class Ex26 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Type a number to sum: ");
+        int sum = 0;
+
+        while (true) {
+            int read = Integer.parseInt(reader.nextLine());
+            if (read == 0) {
+                break;
+            }
+
+        sum += read;
+
+            System.out.println("Sum now: " + sum);
+        }
+
+        System.out.println("Sum in the end: " + sum);
+    }
+}
