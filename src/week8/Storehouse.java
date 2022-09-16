a18fb34b238f45720cf3d919f22e4d82861de2e2
Piret <piret.saar.003@khk.ee>
2022-09-16 17:32:03 +0300

    Week 8 - Ex12.2: Product Stock

--- src/week8/Storehouse.java
+++ src/week8/Storehouse.java
@@ -5,12 +5,15 @@ import java.util.Map;
 
 public class Storehouse {
     private Map<String, Integer> products;
+    private Map<String, Integer> stocks;
 
     public Storehouse() {
         products = new HashMap<String, Integer>();
+        stocks = new HashMap<String, Integer>();
     }
     public void addProduct(String product, int price, int stock) {
         this.products.put(product, price);
+        this.stocks.put(product, stock);
     }
     public int price(String product) {
         if (this.products.containsKey(product)){
@@ -18,5 +21,22 @@ public class Storehouse {
         } else {
             return -99;
         }
+    }
+    public int stock(String product) {
+        if (this.stocks.containsKey(product)){
+            return this.stocks.get(product);
+        } else {
+            return 0;
+        }
+    }
+    public boolean take(String product) {
+        if (this.stocks.containsKey(product)){
+            if (this.stocks.get(product) > 0) {
+                this.stocks.put(product, this.stocks.get(product) - 1);
+                return true;
+            }
+            return false;
+        }
+        return false;
     }
 }
