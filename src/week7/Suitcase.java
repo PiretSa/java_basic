19f55ec67656a1ee2d7b002ab28fa78ff6291dd1
Piret <piret.saar.003@khk.ee>
2022-09-16 11:36:31 +0300

    Week 7 - Ex4.3: Language Check refactored

--- src/week7/Suitcase.java
+++ src/week7/Suitcase.java
@@ -3,33 +3,46 @@ package week7;
 import java.util.ArrayList;
 
 public class Suitcase {
-    private ArrayList<Thing> things;
-    private int weightLimit;
+        private ArrayList<Thing> things;
+        private int weightLimit;
 
-    public Suitcase(int weightLimit) {
-        this.weightLimit = weightLimit;
-        this.things = new ArrayList<Thing>();
-    }
-    public void addThing(Thing thing){
-        int weightCurrent = 0;
-        for (Thing currentThing:this.things) {
-            weightCurrent += currentThing.getWeight();
+        public Suitcase(int weightLimit) {
+            this.weightLimit = weightLimit;
+            this.things = new ArrayList<Thing>();
         }
-        if(weightCurrent + thing.getWeight() <= this.weightLimit){
-            this.things.add(thing);
+
+        public void addThing(Thing thing){
+            int weightCurrent = this.totalWeight();
+            if(weightCurrent + thing.getWeight() <= this.weightLimit) {
+                this.things.add(thing);
+            }
+        }
+
+        @Override
+        public String toString() {
+            String languageControl = "";
+            if(this.things.size() == 0) {
+                languageControl =  "empty";
+            } else if (this.things.size() == 1) {
+                languageControl = this.things.size() + " thing";
+            } else {
+                languageControl = this.things.size() + " things";
+            }
+            return languageControl + " (" + this.totalWeight() + " kg)";
+        }
+
+        public void printThings(){
+            for (Thing thing :this.things) {
+                System.out.println(thing);
+            }
+        }
+
+        public int totalWeight(){
+            int weightCurrent = 0;
+            for (Thing currentThing :this.things) {
+                weightCurrent += currentThing.getWeight();
+            }
+            return weightCurrent;
         }
     }
 
-    @Override
-    public String toString() {
-        int weightCurrent = 0;
-        for (Thing currentThing:this.things) {
-            weightCurrent += currentThing.getWeight();
-        }
-        if(weightCurrent == 0) {
-            return "empty (" + weightCurrent + " kg)";
-        }
-        String langControl = (this.things.size() > 1) ? "things" : "thing";
-        return this.things.size() + langControl + " things (" + weightCurrent + " kg)";
-    }
-}
