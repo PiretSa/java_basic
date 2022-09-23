2fb0cfd7eb5eb1d836e318301a427c972dc430ef
Piret <piret.saar.003@khk.ee>
2022-09-23 09:57:20 +0300

    Week 9 - Ex20.1: Constant Sensor

---
+++ src/week9/measurements/ConstantSensor.java
@@ -0,0 +1,28 @@
+package week9.measurements;
+
+public class ConstantSensor implements Sensor {
+    private int measuredValue;
+    public ConstantSensor(int measuredValue) {
+        this.measuredValue = measuredValue;
+    }
+
+    @Override
+    public boolean isOn() {
+        return true;
+    }
+
+    @Override
+    public void on() {
+
+    }
+
+    @Override
+    public void off() throws IllegalStateException {
+
+    }
+
+    @Override
+    public int measure() {
+        return this.measuredValue;
+    }
+}
